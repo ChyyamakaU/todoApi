7234fb892d4eeedeaa16447671c9671454c2e10e
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-25 00:38:07 +0100

    fix: update server

--- src/server.js
+++ src/server.js
@@ -5,9 +5,14 @@ require("dotenv").config();
 
 const app =require("./app")
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, ()=>
-console.log("server is running"))
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
 
 
