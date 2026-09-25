57ef2ba9f96d646d0f437f28225f144959acb26d
ChyyamakaU <amakaumunna90@gmail.com>
2026-09-25 13:04:11 +0100

    fix:reduce maximum login to 5

--- src/middleware/rateLimit.js
+++ src/middleware/rateLimit.js
@@ -3,7 +3,7 @@ const rateLimit = require("express-rate-limit");
 
 const limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 100,
+    max: 5,
     message: {
         status: "error",
         message: "Too many requests. Please try again later."
